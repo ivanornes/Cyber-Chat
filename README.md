f0d21b86133ae772a81720aafd89e3408dc5ef3a
Ivan <ivan.ornes@gmail.com>
2014-02-14 20:27:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Cyber-Chat
+==========
+
+Chat written in socket.io, Angular and Express
---
+++ public/javascripts/main.js
@@ -0,0 +1,41 @@
+var myApp = angular.module('myApp',[]);
+
+function FirstController($scope){
+    $scope.emitText = function () {
+        var socket = io.connect('http://inventoryx.herokuapp.com:3000');
+        console.log("Emitiendo: "+$scope.data.message);
+        socket.emit('emit',$scope.data.nickname+": "+$scope.data.message);
+        $scope.chat.push("Yourself : "+$scope.data.message);
+        $scope.data.message = "";
+    };
+    $scope.openConnect = function () {
+
+        var socket = io.connect('http://inventoryx.herokuapp.com:3000');
+
+        socket.on('received', function (data) {
+            $scope.chat.push(data);
+            $scope.$apply();
+        });
+    };
+    $scope.chat = [];
+
+
+
+}
+
+myApp.directive('openC', function() {
+    return {
+        restrict: 'E',
+        scope: {
+
+            chat:"="
+        },
+        template: "",
+
+        controller: 'FirstController',
+        link: function (scope, element, attrs) {
+
+            scope.openConnect();
+        }
+    };
+});
